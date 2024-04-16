84219e900bbe3890fe2b51a904f24639e3eb759d
kabandapatrick2580 <sendsome2019@gmail.com>
2024-04-16 19:29:17 +0200

    API_HOST

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -46,7 +46,7 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str):
         """
         Extracts user credentials from a decoded Base64 string
 
